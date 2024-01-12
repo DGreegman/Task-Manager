82114b15936ce1d9287ea9a39312cfa9c431a177
DGreegman <obeagug@gmail.com>
2024-01-12 10:51:40 +0100

    Working on the controllers

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,20 +1,45 @@
 const Task = require('../models/taskModel')
 // get all tasks
 const getAllTasks = async(req, res) =>{
-    res.send('get all tasks')
+    try{
+        const tasks = await Task.find({})
+        return res.status(200).json({tasks})
+    }catch(error){
+       return res.status(500).json({msg: error})
+    }
 }
 
 const createTask = async(req, res) =>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-    res.send('create task')
+    try{
+        const task = await Task.create(req.body)
+        return res.status(201).json({task})
+    }catch(error){
+       return res.status(500).json({msg: error})
+    }
 }
 
+// get a specific task
 const getTask = async(req, res) =>{
-    res.json({id:req.params.id})
+    try {
+
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task) {
+            return res.status(404).json({msg:'No task found'})
+        }
+        return res.status(200).json({task})
+        // const task = await Task.findOne({_id:req.params.id})
+        // if(!task || task === null) {
+        //     return res.status(404).json({msg:'No task found'})
+
+        // }
+    } catch (error) {
+        return res.status(500).json({msg:error.message})
+    }
 }
 
 const updateTask = async(req, res) =>{
+    
     res.send('Update task')
 }
 
